2636a8e27ca60ee439937300595b04cfd1473b39
Andres <24100803@ue.edu.pe>
2026-06-06 03:27:43 -0500

    Login, register y verificacion en AppNavGraph.kt

--- app/src/main/java/com/example/smallchangedam/MainActivity.kt
+++ app/src/main/java/com/example/smallchangedam/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.smallchangedam.navigation.AppNavigation
+import com.example.smallchangedam.presentation.navegation.AppNavGraph
 import com.example.smallchangedam.ui.theme.SmallChangeDAMTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SmallChangeDAMTheme {
-                AppNavigation()
+                AppNavGraph()
             }
         }
     }
