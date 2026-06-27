2ffad23f2f740c2986b8aa3667bef71eb502d06d
Niove-F <niovelbv@gmail.com>
2026-06-27 00:26:53 -0500

    Conexion a backend FINALLY YAAYYY

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,8 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    implementation("androidx.navigation:navigation-compose:2.9.8")
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("androidx.compose.material:material-icons-core:1.7.8")
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
 }
--- app/src/main/java/com/example/smallchangedam/MainActivity.kt
+++ app/src/main/java/com/example/smallchangedam/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.smallchangedam
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.LaunchedEffect
+import com.example.smallchangedam.data.RetrofitClient
 import com.example.smallchangedam.presentation.navegation.AppNavGraph
 import com.example.smallchangedam.ui.theme.SmallChangeDAMTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +18,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SmallChangeDAMTheme {
+
+                // Prueba de fuego de la API al arrancar la app
+                LaunchedEffect(key1 = true) {
+                    try {
+                        // Cambia a un hilo secundario para no trabar la interfaz
+                        withContext(Dispatchers.IO) {
+                            // Intentamos pedir la lista de ofertas al backend de Visual Studio
+                            val ofertas = RetrofitClient.apiService.listarOfertas()
+                            Log.d("API_TEST", "¡Conexión Exitosa! Número de ofertas encontradas: ${ofertas.size}")
+                        }
+                    } catch (e: Exception) {
+                        Log.e("API_TEST", "Error al conectar con la API de Visual Studio", e)
+                    }
+                }
+
                 AppNavGraph()
             }
         }
